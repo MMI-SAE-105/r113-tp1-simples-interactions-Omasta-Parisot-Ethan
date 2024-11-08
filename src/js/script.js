464981abf5f703d6da7fb72df9a455b8efb91e8a
Omasta-Parisot-Ethan <ethan.omasta-parisot@edu.univ-fcomte.fr>
2024-11-08 15:01:21 +0100

    Rendu Final TP 1

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 // TODO
+// Changer la couleur du texte
+
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+aMettreEnRouge.style.color = "red"
+
+
+// Changer la couleur du texte suite à un click
+
+const enRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+enRougeSuiteAClick.addEventListener("click", (evt)=>{
+    enRougeSuiteAClick.style.color = "red"
+});
+
+// Trouver les H2 du document
+
+const collH2 = document.querySelectorAll("h2")
+collH2.forEach((elm) => {
+    elm.addEventListener("click", (evt) => {
+        evt.target.style.color = "red";
+    });
+});
